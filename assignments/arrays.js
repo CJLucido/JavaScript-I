3d2b658367e9462db0b845a00d5df02e715b129a
CJ Lucido <c.jose.lucido@gmail.com>
2019-09-16 12:08:22 -0700

    completed challenge 3 of arrays.js

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -87,8 +87,13 @@ console.log(lastCar.car_make + " and " + lastCar.car_model);
 // ==== Challenge 3 ====
 // The marketing team wants the car models listed alphabetically on the website. Sort all the car model names into alphabetical order and log the results in the console
 let carModels = [];
+for (i = 0; i < inventory.length; i++){
+    carModels.push(inventory[i].car_model); //originally forgot to specify which index I wanted pushed up to carModels
+}
+//console.log(carModels); //this was just a test to make sure it worked
 let carModelsSorted = [];
-console.log();
+carModelsSorted = carModels.sort();
+console.log(carModelsSorted);
 
 // ==== Challenge 4 ====
 // The accounting team needs all the years from every car on the lot. Create a new array from the dealer data containing only the car years and log the result in the console.
