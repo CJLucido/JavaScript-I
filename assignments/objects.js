62ce45c623f3ab8636d1121a088802f1315a4221
CJ Lucido <c.jose.lucido@gmail.com>
2019-09-16 11:26:40 -0700

    added objects to objects.js file for challenge 1

--- assignments/objects.js
+++ assignments/objects.js
@@ -19,6 +19,40 @@ const example = {
 
 // Write your intern objects here:
 
+const employee1 = {
+  id: 1,
+  email: "mmelloy0@psu.edu",
+  name: "Mitzi",
+  gender: "F",
+};
+
+const employee2 = {
+  id: 2,
+  email: "kdiben1@tinpic.com",
+  name: "Kennan",
+  gender: "M",
+}
+
+const employee3 = {
+  id: 3,
+  email: "kmummery2@wikimedia.org",
+  name: "Keven",
+  gender: "M",
+}
+
+const employee4 = {
+  id: 4,
+  email: "gmartinson3@illinois.edu",
+  name: "Gannie",
+  gender: "M",
+}
+
+const employee5 = {
+  id: 5,
+  email: "adaine5@samsung.com",
+  name: "Antonietta",
+  gender: "F",
+}
 
 // ==== Challenge 2: Reading Object Data ==== 
 // Once your objects are created, log out the following requests from HR into the console:
